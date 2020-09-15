b49c6ddf39a4b70307c7215fdfd1b65a26e8e8aa
Arjun Dogra <arjundogra146@gmail.com>
2020-09-15 23:28:18 +0530

    Assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+var LeadersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false 
+    }
+}, {
+    timestamps: true
+})
+
+var Leaders = mongoose.model('Leader', LeadersSchema);
+
+module.exports = Leaders;
