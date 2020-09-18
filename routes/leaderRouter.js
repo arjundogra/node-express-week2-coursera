8d1dbd4fbb83c98f2b529b4eb48de8f73c31175f
Arjun Dogra <arjundogra146@gmail.com>
2020-09-18 20:17:05 +0530

    Passport JWT

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+var authenticate = require('../authenticate');
+
 var Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
@@ -18,7 +20,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err)=>next(err))
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Leaders.create(req.body)
     .then((leader)=>{
         console.log("Data Entered", leader)
@@ -28,11 +30,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.remove()
     .then((resp) => {
         res.statusCode = 200;
@@ -56,18 +58,18 @@ leaderRouter.route('/:leaderId')
 res.statusCode = 403;
 res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, { new: true })
-    .then((leader) => {
+    .then(authenticate.verifyUser, (leader) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(leader);
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((resp) => {
         res.statusCode = 200;
